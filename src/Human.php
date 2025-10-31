0f4aa5dfd015108140e37c79c6fa9fc3c6909df4
Imam Janjua <i.janjua@simplicity.ag>
2025-10-31 16:43:59 +0100

    Merle

--- src/Human.php
+++ src/Human.php
@@ -2,9 +2,22 @@
 
 class Human 
 {
-    public function __construct(private string $firstname, private string $lastname,        private string $age, private string $profession)
-    {
+  private string $firstname;
+  private string $lastname;
+  private string $age; 
+  private string $profession;
 
+  public function __construct(string $firstname, string $lastname, string $age, string $profession)
+  {
+    $this->validateFirstName($firstname);
+    $this->validateLastName($lastname);
+    $this->validateAge($age);
+    $this->validateProfession($profession);
+
+      $this->firstname = $firstname;
+      $this->lastname = $lastname;
+      $this->age = $age;
+      $this->profession = $profession;
      }
 
     public function sayName()
@@ -26,4 +39,78 @@ class Human
     {
          echo "\n $input .";   
     }
+
+  private function validateFirstName(string $firstname)
+  {
+    if (!$firstname)
+    {
+       throw new \Exception('Firstname is false');
+    }
+
+    if (empty($firstname))
+    {
+      throw new \Exception('Firstname is empty');
+    }
+
+    if (is_numeric($firstname))
+    {
+      throw new \Exception('Firstname is a number');
+    }
+  }
+
+    private function validateLastName(string $lastname)
+  {
+    if (!$lastname)
+    {
+       throw new \Exception('lastname is false');
+    }
+
+    if (empty($lastname))
+    {
+      throw new \Exception('lastname is empty');
+    }
+
+    if (is_numeric($lastname))
+    {
+      throw new \Exception('lastname is a number');
+    }
+    }
+
+  private function validateAge(string $age)
+  {
+    if (!$age)
+    {
+       throw new \Exception('age is false');
+    }
+
+    if (empty($age))
+    {
+      throw new \Exception('age is empty');
+    }
+
+    if (!is_numeric($age))
+    {
+      throw new \Exception('age is not a number');
+    }
+  }
+
+  private function validateProfession(string $profession)
+  {
+    if (!$profession)
+    {
+       throw new \Exception('profession is false');
+    }
+
+    if (empty($profession))
+    {
+      throw new \Exception('profession is empty');
+    }
+
+    if (is_numeric($profession))
+    {
+      throw new \Exception('profession is a number');
+    }
+  }
+
+
 } 
--- src/hello-world.php
+++ src/hello-world.php
@@ -1,8 +1,7 @@
 <?php
 declare (strict_types=1);
-include "Human.php"
+include "Human.php";
 
-$human = Human();    
 
 for ($i = 1; $i <= 3; $i++) {
     $firstname = readline('Vorname?: ');
@@ -10,25 +9,10 @@ for ($i = 1; $i <= 3; $i++) {
     $age = readline('Alter?: ');
     $profession = readline('Beruf?: ');
 
-    if (!$age || !$firstname || !$lastname || !$profession) {
-        throw new \Exception('Fehler');
-    }
+    $human = new Human($firstname, $lastname, $age, $profession);
 
-    if (is_numeric($firstname) || is_numeric($lastname) || is_numeric($profession)) {
-        throw new \Exception('Fehler: Zahl!!!'); 
-    }
-
-    if (!is_numeric($age)) {
-        throw new \Exception('Is not a number');
-    }
-
-    if (empty($firstname) || empty($lastname) || empty($age) || empty($profession)) {
-        throw new \Exception('Not allowed to be empty');
-    }
-
-    $human->sayName($firstname, $lastname);
-    $human->sayAge((int)$age);
-    $human->sayProfession($profession);
-
+    $human->sayName();
+    $human->sayAge();
+    $human->sayProfession();
 }
 
