5d15b4ec993bc1426ca3270eb05b16087686bb64
merle-pallenberg-simplicity <m.pallenberg@simplicity.ag>
2025-11-14 14:50:15 +0100

    Lord Marc

--- src/hello-world.php
+++ src/hello-world.php
@@ -2,8 +2,10 @@
 
 declare (strict_types=1);
 include "Human.php";
+include "merchant.php";
 
-$human = new Human($firstname = "Lukas", $lastname = "sir", $age = "11", $profession = "JC");
+$human = new Human(firstname: "Lukas", lastname: "sir", age: "11", profession: "JC");
 echo $human->getFirstName();
 
-
+$merchant = new Merchant("Marc", "Lord", "33", "Zoo");
+echo $merchant->getLastName();
